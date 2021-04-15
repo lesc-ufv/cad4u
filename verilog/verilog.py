99cdfab4fb18ee6a0b36cdf2eca0020eaade42fd
jeronimopenha <jeronimopenha@gmail.com>
2021-04-14 22:27:36 -0300

    Update verilog.py

--- verilog/verilog.py
+++ verilog/verilog.py
@@ -140,8 +140,7 @@ class VERILOGPlugin(Magics):
                 exec(l.replace('=', '+='))
                 #exec(l)
         if flag_op_dict == False:
-            op_dict = [[{'0':'??'}]]
-            
+            exec('[{"0":"??"}]'.replace('=', '+='))
         #print(op_dict, sign_list, time_begin,time_end, base)
         vcd_plt = VcdPlotter('/content/%s'%name)
         vcd_plt.show(op_dict, sign_list, time_begin[0], time_end[0], base[0])
